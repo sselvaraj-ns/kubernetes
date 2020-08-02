095ac7d3bdd016c97268f75a70223578246258b6
annamalaipon <annamalaipon@gmail.com>
2020-08-02 23:04:41 +0530

    Adding cert

--- install_tls_cert.sh
+++ install_tls_cert.sh
@@ -1,3 +1,6 @@
+#Step0: Make cert directory
+mkdir /tmp/cert 
+
 #Root Public Certificate should be available in all components
 
 #Step1: Create local server as a CA server for generating users certification.
@@ -67,4 +70,82 @@ openssl genrsa -out /tmp/cert/etcd-client.key 2048
 openssl req -new -key /tmp/cert/etcd-client.key -subj "/CN=etcd-client" -out /tmp/cert/etcd-client.csr
 
 #Step12: Sign the csr requested certificate and provide a publick key along with certificate
-openssl x509 -req -days 30 -in /tmp/cert/etcd-client.key -CA /tmp/cert/ca.crt -CAkey /tmp/cert/ca.key -out /tmp/cert/etcd-client.crt
+openssl x509 -req -days 30 -in /tmp/cert/etcd-client.csr -CA /tmp/cert/ca.crt -CAkey /tmp/cert/ca.key -out /tmp/cert/etcd-client.crt
+
+#Step13: Genereate Kube-API Server Private key
+#Use OPENSSL tool to generate the private key.
+openssl genrsa -out /tmp/cert/kube-api.key 2048
+
+#Step14: Generate Kube-api Conf
+cat > /tmp/cert/kube-api.cnf << EOF
+[req]
+req_extensions = v3_req
+default_bits = 2048
+distinguished_name = dn
+prompt             = no
+
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+
+[dn]
+C="IN"
+ST="Karnataka"
+L="Bangalore"
+O="Self"
+OU="Learning"
+CN="etcd"
+
+[alt_names]
+DNS.0 = kubernetes
+DNS.1 = kubernetes.default
+DNS.2 = kubernetes.default.svc
+DNS.3 = kubernetes.default.svc.cluster.local
+IP.0 = 127.0.0.1
+IP.1 = 10.32.0.1
+IP.2 = ${SERVER_IP}
+EOF
+
+#Step15: Generate the Certificate signing request.
+openssl req -new -key /tmp/cert/kube-api.key -subj "/CN=api-server" -out /tmp/cert/kube-api.csr -config /tmp/cert/kube-api.cnf
+
+#Step16: Sign the csr requested certificate and provide a publick key along with certificate
+openssl x509 -req -days 30 -in /tmp/cert/kube-api.csr \
+-CA /tmp/cert/ca.crt \
+-CAkey /tmp/cert/ca.key \
+-CAcreateserial \
+-out /tmp/cert/kube-api.crt \
+-extensions v3_req \
+-extfile /tmp/cert/kube-api.cnf
+
+#Step17: Genereate Service Account Private key
+#Use OPENSSL tool to generate the private key.
+openssl genrsa -out /tmp/cert/service-account.key 2048
+
+#Step18: Generate the Certificate signing request.
+openssl req -new -key /tmp/cert/service-account.key -subj "/CN=service-account" -out /tmp/cert/service-account.csr
+
+#Step19: Sign the csr requested certificate and provide a publick key along with certificate
+openssl x509 -req -days 30 -in /tmp/cert/service-account.csr -CA /tmp/cert/ca.crt -CAkey /tmp/cert/ca.key -out /tmp/cert/service-account.crt
+
+
+#Step20: Genereate kube-api-etcd-client private key
+#Use OPENSSL tool to generate the private key.
+openssl genrsa -out /tmp/cert/apiserver-etcd-client.key 2048
+
+#Step21: Generate the Certificate signing request.
+openssl req -new -key /tmp/cert/apiserver-etcd-client.key -subj "/CN=apiserver-etcd-client" -out /tmp/cert/apiserver-etcd-client.csr
+
+#Step22: Sign the csr requested certificate and provide a publick key along with certificate
+openssl x509 -req -days 30 -in /tmp/cert/apiserver-etcd-client.csr -CA /tmp/cert/ca.crt -CAkey /tmp/cert/ca.key -out /tmp/cert/apiserver-etcd-client.crt
+
+#Step23: Genereate kube-api-kubelet private key
+#Use OPENSSL tool to generate the private key.
+openssl genrsa -out /tmp/cert/apiserver-kubelet-client.key 2048
+
+#Step24: Generate the Certificate signing request.
+openssl req -new -key /tmp/cert/apiserver-kubelet-client.key -subj "/CN=apiserver-kubelet-client" -out /tmp/cert/apiserver-kubelet-client.csr
+
+#Step25: Sign the csr requested certificate and provide a publick key along with certificate
+openssl x509 -req -days 30 -in /tmp/cert/apiserver-kubelet-client.csr -CA /tmp/cert/ca.crt -CAkey /tmp/cert/ca.key -out /tmp/cert/apiserver-kubelet-client.crt
--- starting_service.sh
+++ starting_service.sh
@@ -1,15 +1,66 @@
-
 #Step1: Starting ETCD service locally
 /tmp/etcd-download-test/etcd --name etcd \
 --cert-file=/tmp/cert/etcd.crt \
 --key-file=/tmp/cert/etcd.key \
 --trusted-ca-file=/tmp/cert/ca.crt \
 --client-cert-auth \
---advertise-client-urls=https://127.0.0.1:2379 \
---listen-client-urls=https://127.0.0.1:2379
+--advertise-client-urls=https://0.0.0.0:2379 \
+--listen-client-urls=https://0.0.0.0:2379 \
+&
 
-#Step2: Add data in etcd
-/tmp/etcd-download-test/etcdctl --cacert /tmp/cert/ca.crt --cert /tmp/cert/etcd-client.crt --key /tmp/cert/etcd-client.key put name anna
+# #Step2: Starting Kube-api service locally
+# /tmp/kubernetes/server/bin/kube-apiserver \
+# --advertise-address=${SERVER_IP} \
+# --allow-privileged=true \
+# --authorization-mode=Node,RBAC \
+# --insecure-port=0 \
+# --secure-port=6443 \
+# --service-cluster-ip-range=10.32.0.0/24 \
+# --enable-bootstrap-token-auth=true \
+# ### OWN
+# --tls-cert-file=/tmp/cert/kube-api.crt \
+# --tls-private-key-file=/tmp/cert/kube-api.key \
+# ### ETCD
+# --etcd-certfile=/tmp/cert/apiserver-etcd-client.crt \
+# --etcd-keyfile=/tmp/cert/apiserver-etcd-client.key \
+# ### SA
+# --service-account-key-file=/tmp/cert/service-account.key \
+# ### kubelet
+# --kubelet-client-certificate=/tmp/cert/apiserver-etcd-client.crt \
+# --kubelet-client-key=/tmp/cert/apiserver-etcd-client.key \
+# ### Public keys
+# --client-ca-file=/tmp/cert/ca.crt \
+# --etcd-cafile=/tmp/cert/ca.crt \
 
-#Step3: Retrive data from etcd
+#Step2: Starting Kube-api service locally
+/tmp/kubernetes/server/bin/kube-apiserver \
+--advertise-address=${SERVER_IP} \
+--allow-privileged=true \
+--authorization-mode=Node,RBAC \
+--insecure-port=0 \
+--secure-port=6443 \
+--etcd-servers=https://127.0.0.1:2379 \
+--service-cluster-ip-range=10.32.0.0/24 \
+--enable-bootstrap-token-auth=true \
+--tls-cert-file=/tmp/cert/kube-api.crt \
+--tls-private-key-file=/tmp/cert/kube-api.key \
+--etcd-certfile=/tmp/cert/apiserver-etcd-client.crt \
+--etcd-keyfile=/tmp/cert/apiserver-etcd-client.key \
+--service-account-key-file=/tmp/cert/service-account.key \
+--kubelet-client-certificate=/tmp/cert/apiserver-etcd-client.crt \
+--kubelet-client-key=/tmp/cert/apiserver-etcd-client.key \
+--client-ca-file=/tmp/cert/ca.crt \
+--etcd-cafile=/tmp/cert/ca.crt 
+
+#Step3: Add data in etcd
+/tmp/etcd-download-test/etcdctl \
+--cacert /tmp/cert/ca.crt \
+--cert /tmp/cert/etcd-client.crt \
+--endpoints https://127.0.0.1:2379 \
+--key /tmp/cert/etcd-client.key \
+put name anna
+
+#Step4: Retrive data from etcd
 /tmp/etcd-download-test/etcdctl --cacert /tmp/cert/ca.crt --cert /tmp/cert/etcd-client.crt --key /tmp/cert/etcd-client.key get name
+
+
